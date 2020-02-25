65cb0e42f450fb456a62e0b65065c5c06b1b6676
Dmytro Tsapko <dmytro.tsapko@gmail.com>
2020-02-25 22:39:57 +0200

    eslint example was added

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import User from './user';
+
+const u = new User(33, 'qdqdqwdqwd');
+const r = () => { console.log(u); };
+console.log(u);
+
+document.querySelector('body').addEventListener('click', () => {
+  r();
+});
---
+++ src/user/index.js
@@ -0,0 +1,4 @@
+export default function User(age, name) {
+    this.age = age;
+    this.name = name;
+}
